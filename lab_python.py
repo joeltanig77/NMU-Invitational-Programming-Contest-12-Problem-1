2e5b1192b7d41b828e8d34429fb2c40176e77b97
Joel Tanig <joeltanig77@gmail.com>
2021-03-10 08:53:14 -0800

    Completed

--- lab_python.py
+++ lab_python.py
@@ -1,28 +1,29 @@
 def main():
-    leftRightSideRectangle = ""
-    ringString1 = "-"
-    ringString2 = "|"
-    coreString1 = "/"
-    coreString2 = "\\"
-    newLine = "\n"
-    space = " "
-    backSpace = "\b"
-    alignmentForUpDown = ""
-
-    rings = int(input("Enter the number of rings you would like "))
-
-    #Base Cases
-    if rings < 0:
-        print("The number of rings has to be greater than zero")
-        exit(-1)
-
-    coreSize = int(input("Enter the number of cores you would like "))
-
-    if coreSize < 0:
-        print("The core size cannot be negative")
-        exit(-1)
-
     while True:
+        ringString1 = "-"
+        ringString2 = "|"
+        coreString1 = "/"
+        coreString2 = "\\"
+        space = " "
+        backSpace = "\b"
+        alignmentForUpDown = ""
+
+        rings = int(input("Enter the number of rings you would like "))
+
+        #Base Cases
+        if rings < 0:
+            print("The number of rings has to be greater than zero")
+            exit(-1)
+
+        coreSize = int(input("Enter the number of cores you would like "))
+
+        if coreSize < 0:
+            print("The core size cannot be negative")
+            exit(-1)
+
+        if rings == 0 and coreSize == 0:
+            break
+
         print(f"Rings: {rings}")
         print(f"Core Size: {coreSize}")
         horizontalSpace = coreSize*space
@@ -30,82 +31,55 @@ def main():
 
 
         #Alignment for upper rectangle
-        if coreSize % 2 == 0:
-            spaceCounter = coreSize/2
-            while spaceCounter != 0:
-                alignmentForUpDown += " "
-                spaceCounter -= 1
-        else:
-            spaceCounter = rings
-            while spaceCounter != 0:
-                alignmentForUpDown += " "
-                spaceCounter -= 1
+        spaceCounter = rings
+        while spaceCounter != 0:
+            alignmentForUpDown += " "
+            spaceCounter -= 1
 
         counter = rings
         fowardSlash = coreString1
         backSlash = coreString2
         ringsCounter = 1
         backSpaceCounter = 0
-        #if its a even core.
+        #Top part of Rectangle
         while counter != 0:
-            if coreSize % 2 == 0:
-                backSpace = backSpace*(backSpaceCounter) #Delete +1 for odd
-            #backSpaceTESTEVEN = backSpace*(backSpaceCounter)
-            else:
-                backSpace = backSpace * (backSpaceCounter+1)
+            backSpace = backSpace * (backSpaceCounter+1)
             hyphin = ringString1 * coreSize
-
             print(f'{alignmentForUpDown}{backSpace}{fowardSlash}{hyphin}{backSlash}')
             backSpace = '\b'
-
             backSpaceCounter += 1
             counter -= 1
             ringsCounter += 1
             fowardSlash = coreString1 * (ringsCounter)
             backSlash = coreString2 * (ringsCounter)
 
+
         counter = coreSize
         while counter != 0:
-            leftRightSideRectangle = verticalLines + horizontalSpace + verticalLines
-            print(leftRightSideRectangle)
+            printCore = verticalLines + horizontalSpace + verticalLines
+            print(printCore)
             counter -= 1
 
-        #Alignment for lower rectangle
-        alignmentForUpDown = ""
-        if coreSize % 2 == 0:
-            spaceCounter = coreSize/2
-            while spaceCounter != 0:
-                alignmentForUpDown += " "
-                spaceCounter -= 1
-        else:
-            spaceCounter = rings
-            while spaceCounter != 0:
-                alignmentForUpDown += " "
-                spaceCounter -= 1
-
+        #Bottom part of rectangle
         counter = rings
-        ringsCounter = rings
-        fowardSlash = coreString1 * (ringsCounter)
-        backSlash = coreString2 * (ringsCounter)
-        ringsCounter = rings
+        backSpaceCounter = 0
+        fowardSlash = coreString1 * (rings)
+        backSlash = coreString2 * (rings)
+        backSpace = '\b'
+        space = ""
         while counter != 0:
+            backSpace = backSpace * (backSpaceCounter+1)
             hyphin = ringString1 * coreSize
-            print(f'{alignmentForUpDown}{backSlash}{hyphin}{fowardSlash}')
+            print(f'{backSpace}{space}{backSlash}{hyphin}{fowardSlash}')
+            backSpace = '\b'
+            space += " "
+            backSpaceCounter += 1
+            fowardSlash = coreString1 * (counter-1)
+            backSlash = coreString2 * (counter-1)
             counter -= 1
-            ringsCounter -= 1
-            fowardSlash = coreString1 * ringsCounter
-            backSlash = coreString2 * ringsCounter
 
-        break
-
-
-
-    #print(f"Rings: {rings}")
-    #print(f"Core Size: {coreSize}")
-
-
-
-
+    print(f"Rings: {rings}")
+    print(f"Core Size: {coreSize}")
 
 
 if __name__ == '__main__':
